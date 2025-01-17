80c41b24e8c5f28eccf7f35c7b3d8c68d9aad8d1
Melissa Gwyn <melissagwyn@gmail.com>
2025-01-17 13:24:34 -0500

    finished sign in feature, used react route navigate state to render message only when new user is created

--- src/pages/auth/SignInPage.jsx
+++ src/pages/auth/SignInPage.jsx
@@ -1,34 +1,54 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import AuthForm from "./AuthForm";
 import FormContainer from "./FormContainer";
+import * as userService from "services/user";
 
 const SignInPage = () => {
-  const [error, setError]= useState('')
-  return ( 
+  const [error, setError] = useState("");
+  const location = useLocation();
+  return (
     <FormContainer>
       <div className="text-red-800 font-lato">{error}</div>
-    <AuthForm
-      fields={[
-        {
-          label: "username",
-          type: "text",
-        },
-        {
-          label: "password",
-          type: "password",
-        },
-      ]}
-      submitButtonLabel="sign in"
-      onSubmit={(values)=>{
-        if (values != values) {
-          setError('username or password does not match');
-          return;
-        }
-      }}
-    />
-    <Link to="/sign-up" className="text-sm text-emerald-800 underline">create an account</Link>
+      {location.state?.accountCreated && (
+        <div className="p-4 mb-6 border border-emerald-700 bg-emerald-200 rounded-lg text-emerald-800 font-lato">
+          Account created successfully. Please sign in.
+        </div>
+      )}
+      <AuthForm
+        fields={[
+          {
+            label: "username",
+            type: "text",
+          },
+          {
+            label: "password",
+            type: "password",
+          },
+        ]}
+        submitButtonLabel="sign in"
+        onSubmit={async (values) => {
+          if (values != values) {
+            setError("username or password does not match");
+            return;
+          }
+          const response = await userService.createSession({
+            username: values.username,
+            password: values.password,
+          });
+          if (response.status === 201) {
+            setError("");
+            console.log("log in successful!");
+          } else {
+            const data = await response.json();
+            setError(data.error);
+          }
+        }}
+      />
+      <Link to="/sign-up" className="text-sm text-emerald-800 underline">
+        create an account
+      </Link>
     </FormContainer>
- );
+  );
 };
 export default SignInPage;
--- src/pages/auth/SignUpPage.jsx
+++ src/pages/auth/SignUpPage.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthForm from "./AuthForm";
 import FormContainer from "./FormContainer";
 import * as userService from "services/user";
 
 const SignUpPage = () => {
   const [error, setError] = useState("");
+  const navigate = useNavigate()
   return (
     <FormContainer>
       <div className="text-red-800 font-lato">{error}</div>
@@ -44,7 +45,12 @@ const SignUpPage = () => {
           });
           if (response.status === 201) {
             setError('')
-            console.log("user created!");
+            navigate('/',{
+              state:{
+                accountCreated:true
+              }
+            })
+            
           } else {
             const data = await response.json();
             setError(data.error);
